8d6978228f3131eca6ff210216f657bc89dbcc03
Gokul N K <nkgokul@gmail.com>
2018-08-17 12:34:01 +0530

    Added chalk and minor css changes

---
+++ learnings/index.js
@@ -0,0 +1,14 @@
+const chalk = require("chalk");
+const path = require("path");
+console.log(chalk);
+console.log("Chalk uploaded without any issues");
+console.log(chalk.green("Somethign in green"));
+console.log(chalk.red("Somethign in red"));
+
+/**
+const ownPath = path.ownPath;
+const appPath = path.appPath;
+*/
+
+console.log(path);
+//console.log(appPath);
